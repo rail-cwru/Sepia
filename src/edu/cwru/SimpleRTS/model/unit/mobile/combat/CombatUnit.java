efa11c3415c7fc99dd64fbdadddd3580bfa84c8f
Timothy Ernsberger <tse6@case.edu>
2011-06-07 00:53:32 +0000

    changed CombatUnit and NonCombatUnit to interfaces

--- src/edu/cwru/SimpleRTS/model/unit/mobile/combat/CombatUnit.java
+++ src/edu/cwru/SimpleRTS/model/unit/mobile/combat/CombatUnit.java
@@ -1,7 +1,5 @@
 package edu.cwru.SimpleRTS.model.unit.mobile.combat;
 
-import edu.cwru.SimpleRTS.model.unit.mobile.MobileUnit;
-
-public class CombatUnit extends MobileUnit {
+public interface CombatUnit {
 
 }
--- src/edu/cwru/SimpleRTS/model/unit/mobile/noncombat/NonCombatUnit.java
+++ src/edu/cwru/SimpleRTS/model/unit/mobile/noncombat/NonCombatUnit.java
@@ -1,7 +1,5 @@
 package edu.cwru.SimpleRTS.model.unit.mobile.noncombat;
 
-import edu.cwru.SimpleRTS.model.unit.mobile.MobileUnit;
-
-public class NonCombatUnit extends MobileUnit {
+public interface NonCombatUnit {
 
 }
