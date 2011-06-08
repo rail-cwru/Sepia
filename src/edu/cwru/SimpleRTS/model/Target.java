d757733a7169de6644894157b60f8df9be834edc
Timothy Ernsberger <tse6@case.edu>
2011-06-08 00:57:32 +0000

    added class description comments

--- src/edu/cwru/SimpleRTS/model/Target.java
+++ src/edu/cwru/SimpleRTS/model/Target.java
@@ -1,5 +1,9 @@
 package edu.cwru.SimpleRTS.model;
-
+/**
+ * An empty interface that signifies that an implementing class can be the direct object of an action.
+ * @author Tim
+ *
+ */
 public interface Target {
 
 }
--- src/edu/cwru/SimpleRTS/model/Template.java
+++ src/edu/cwru/SimpleRTS/model/Template.java
@@ -1,6 +1,15 @@
 package edu.cwru.SimpleRTS.model;
-
+/**
+ * Signifies that an implementing class provides generic details about a specific object.
+ * Also provides a means for creating factory methods for specific kinds of game objects.
+ * @author Tim
+ *
+ * @param <T>
+ */
 public interface Template<T> {
-	
+	/**
+	 * A factory method that produces copies of a "default" object
+	 * @return
+	 */
 	public T produceInstance();
 }
