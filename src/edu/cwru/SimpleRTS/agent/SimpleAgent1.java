184d21fd92b845f91d57481f843d4e492a9e25dd
Timothy Ernsberger <tse6@case.edu>
2011-07-23 18:20:33 +0000

    Changed visibility on constructor.

--- src/edu/cwru/SimpleRTS/agent/SimpleAgent1.java
+++ src/edu/cwru/SimpleRTS/agent/SimpleAgent1.java
@@ -20,9 +20,8 @@ import edu.cwru.SimpleRTS.model.unit.Unit.UnitView;
  */
 public class SimpleAgent1 extends Agent {
 
-	protected SimpleAgent1(int playernum) {
+	public SimpleAgent1(int playernum) {
 		super(playernum);
-		// TODO Auto-generated constructor stub
 	}
 
 	StateView currentState;
