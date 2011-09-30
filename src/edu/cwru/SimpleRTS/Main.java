4a286b9644f7a677b142cb8c67e7e4ce5d34b545
Timothy Ernsberger <tse6@case.edu>
2011-09-30 22:49:33 +0000

    Added a reference to Preferences to get number of episodes.

--- src/edu/cwru/SimpleRTS/Main.java
+++ src/edu/cwru/SimpleRTS/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.prefs.Preferences;
 
 import edu.cwru.SimpleRTS.agent.Agent;
 import edu.cwru.SimpleRTS.environment.Environment;
@@ -76,6 +77,8 @@ public class Main {
 				}
 			}			
 		}
+		Preferences prefs = Preferences.userRoot().node("eecs/cwru/edu/SimpleRTS/environment");
+		prefs.getInt("NumEpisodes", 1);
 		Model model = new SimpleModel(initState, 6);
 		Environment env = new Environment(agents.toArray(new Agent[0]),model);
 		for(int episode = 0; episode < 100; episode++)
@@ -86,7 +89,7 @@ public class Main {
 	}
 	private static void printUsageAndExit(String error) {
 		System.out.println(error);
-		System.out.println("Usage: java [-cp <path to your agent's class file>] -jar SimpleRTS.jar <model file name> <number of episodes> [[--agent <agent class name> <player number> [--loadfrom <serialized agent file name>]] ...] ");
+		System.out.println("Usage: java [-cp <path to your agent's class file>] -jar SimpleRTS.jar <model file name> [[--agent <agent class name> <player number> [--loadfrom <serialized agent file name>]] ...] ");
 		System.out.println("\nExample: java -jar SimpleRTS.jar data/map1 10 --agent SimpleAgent1 --agent SimpleAgent1");
 		System.out.println("\tThis will load the map stored in the file data/map1 with two new instances of SimpleAgent1 and run 10 episodes");
 		System.out.println("Example: java -jar SimpleRTS.jar data/map1 1000 --agent ScriptedGoalAgent --loadfrom agents/script1 --agent SimpleAgent2");
