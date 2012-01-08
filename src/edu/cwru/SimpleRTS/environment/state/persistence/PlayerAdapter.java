c233179f55c59167332512a7df1f735964539be1
Timothy Ernsberger <tse6@case.edu>
2012-01-08 18:05:40 +0000

    Created an apdapter for PlayerState. Filled in StateAdapter.

---
+++ src/edu/cwru/SimpleRTS/environment/state/persistence/PlayerAdapter.java
@@ -0,0 +1,82 @@
+package edu.cwru.SimpleRTS.environment.state.persistence;
+
+import java.util.List;
+
+import edu.cwru.SimpleRTS.environment.PlayerState;
+import edu.cwru.SimpleRTS.environment.state.persistence.generated.XmlPlayer;
+import edu.cwru.SimpleRTS.environment.state.persistence.generated.XmlResourceQuantity;
+import edu.cwru.SimpleRTS.environment.state.persistence.generated.XmlUnit;
+import edu.cwru.SimpleRTS.model.Template;
+import edu.cwru.SimpleRTS.model.resource.ResourceType;
+import edu.cwru.SimpleRTS.model.unit.Unit;
+
+public class PlayerAdapter {
+	
+
+	public XmlPlayer toXml(PlayerState player) {
+		XmlPlayer xml = new XmlPlayer();
+		UnitAdapter unitAdapter = new UnitAdapter(player.getTemplates());
+		
+		xml.setID(player.playerNum);
+		
+		List<XmlUnit> units = xml.getUnit();
+		for(Unit u : player.getUnits().values())
+		{
+			units.add(unitAdapter.toXml(u));
+		}
+		
+		List<Integer> upgrades = xml.getUpgrade();
+		upgrades.addAll(player.getUpgrades());
+		
+		List<Integer> templateIds = xml.getTemplate();
+		templateIds.addAll(player.getTemplates().keySet());
+				
+		List<XmlResourceQuantity> resources = xml.getResourceAmount();
+		for(ResourceType type : ResourceType.values())
+		{
+			XmlResourceQuantity quantity = new XmlResourceQuantity();
+			quantity.setType(type);
+			quantity.setQuantity(player.getCurrentResourceAmount(type));
+			resources.add(quantity);
+		}
+
+		xml.setSupply(player.getCurrentSupply());
+		xml.setSupplyCap(player.getCurrentSupplyCap());
+		
+		return xml;
+	}
+	
+	public PlayerState fromXml(XmlPlayer xml, @SuppressWarnings("rawtypes") List<Template> templates) {
+		PlayerState player = new PlayerState(xml.getID());
+		UnitAdapter unitAdapter = new UnitAdapter(player.getTemplates());
+		
+		for(XmlUnit unit : xml.getUnit())
+		{
+			player.addUnit(unitAdapter.fromXml(unit));
+		}
+		
+		player.getUpgrades().addAll(xml.getUpgrade());
+		
+		for(int id : xml.getTemplate())
+		{
+			for(@SuppressWarnings("rawtypes") Template template : templates)
+			{
+				if(template.ID == id)
+				{
+					player.addTemplate(template);
+					break;
+				}
+			}
+		}
+		
+		for(XmlResourceQuantity resource : xml.getResourceAmount())
+		{
+			player.setCurrentResourceAmount(resource.getType(), resource.getQuantity());
+		}
+		
+		player.setCurrentSupply(xml.getSupply());
+		player.setCurrentSupplyCap(xml.getSupplyCap());
+		
+		return player;
+	}
+}
--- src/edu/cwru/SimpleRTS/environment/state/persistence/StateAdapter.java
+++ src/edu/cwru/SimpleRTS/environment/state/persistence/StateAdapter.java
@@ -1,5 +1,58 @@
 package edu.cwru.SimpleRTS.environment.state.persistence;
 
-public class StateAdapter {
+import java.util.List;
+import java.util.Map;
 
+import edu.cwru.SimpleRTS.environment.PlayerState;
+import edu.cwru.SimpleRTS.environment.State;
+import edu.cwru.SimpleRTS.environment.State.StateBuilder;
+import edu.cwru.SimpleRTS.environment.state.persistence.generated.XmlPlayer;
+import edu.cwru.SimpleRTS.environment.state.persistence.generated.XmlResourceNode;
+import edu.cwru.SimpleRTS.environment.state.persistence.generated.XmlState;
+import edu.cwru.SimpleRTS.model.Template;
+import edu.cwru.SimpleRTS.model.resource.ResourceNode;
+
+public class StateAdapter {
+	
+	PlayerAdapter playerAdapter = new PlayerAdapter();
+	ResourceNodeAdapter resourceNodeAdapter = new ResourceNodeAdapter();
+
+	public XmlState toXml(State state) {
+		XmlState xml = new XmlState();
+		
+		List<XmlPlayer> players = xml.getPlayer();
+		for(PlayerState ps : state.getPlayerStates())
+		{
+			players.add(playerAdapter.toXml(ps));
+		}
+		
+		List<XmlResourceNode> resources = xml.getResourceNode();
+		for(ResourceNode rn : state.getResources())
+		{
+			resources.add(resourceNodeAdapter.toXml(rn));
+		}
+		
+		xml.setXExtent(state.getXExtent());		
+		xml.setYExtent(state.getYExtent());
+		
+		return xml;
+	}
+	
+	public State fromXml(XmlState xml, @SuppressWarnings("rawtypes") Map<Integer,List<Template>> templates) {
+		StateBuilder state = new StateBuilder();
+		
+		for(XmlPlayer player : xml.getPlayer())
+		{
+			state.addPlayer(playerAdapter.fromXml(player, templates.get(player.getID())));
+		}
+		
+		for(XmlResourceNode resource : xml.getResourceNode())
+		{
+			state.addResource(resourceNodeAdapter.fromXml(resource));
+		}
+		
+		state.setSize(xml.getXExtent(), xml.getYExtent());
+		
+		return state.build();
+	}
 }
