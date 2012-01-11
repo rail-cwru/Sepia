e7f9e79e83ed9ffb47ee55ccf9b10113a8a723d7
Feng Cao <fxc100@case.edu>
2012-01-11 22:54:27 +0000

    modify test_SimpleRTS.m

--- matlab_script/test_SimpleRTS.m
+++ matlab_script/test_SimpleRTS.m
@@ -1,21 +1,14 @@
-% This doesn't work in Mac OS X with Matlab R2011b.
-% Seems like in Mac OS X, matlab R2011b is using its default Java, which
-% doesn't seems parsing the *Config.xml file correctly.
+% Note that this works under Windoes 7.
+% But doesn't work properly under Max OS X and Linux.
 
-% However, this does work under Windoes 7. 
-% -- Feng, 2012.01.06
+clear java  % this command doesn't work properly under Mac OS X, but works under Windows 7. But doesn't matter a lot.
 
-clear java  % doesn't work properly under Mac OS X, but works under Windows 7.
-javaaddpath({[fileparts(pwd),'/bin'], [fileparts(pwd),'/lib/matlabcontrol-4.0.0.jar']});
-%javaclasspath
+%javaaddpath({[fileparts(pwd),'/SimpleRTSv3.1.jar']});
+javaaddpath({[fileparts(pwd),'/bin'], [fileparts(pwd),'/lib/matlabcontrol-4.0.0.jar']}); % use this line under Eclipse project
+ 
+%javaclasspath % print out current java class path
+
 import edu.cwru.SimpleRTS.*;
-
-%cd(pwd);
-%fileattrib('data/midasConfig.xml');
-
-%import java.util.prefs.Preferences;
-%import java.io.FileInputStream;
-%Preferences.importPreferences(FileInputStream('data/midasConfig.xml'));
 
 arg = '--config ../data/midasConfig.xml ../data/rc_3m5t.map --agent edu.cwru.SimpleRTS.agent.MatlabAgent 0 --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam false --agentparam true';
 %arg = '--config ../data/midasConfig.xml ../data/rc_3m5t.map --agent edu.cwru.SimpleRTS.agent.MatlabAgent 0';
