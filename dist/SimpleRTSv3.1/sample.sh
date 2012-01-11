8133e9bfcc6a5bab0c0a388fd5f851bd8a1cd713
Feng Cao <fxc100@case.edu>
2012-01-11 22:44:09 +0000

    Added a bash script: dist/SimpleRTSv3.1/sample.sh

---
+++ dist/SimpleRTSv3.1/sample.sh
@@ -0,0 +1,4 @@
+# sample terminal command to run the java package.
+
+java -cp SimpleRTSv3.1.jar edu.cwru.SimpleRTS.Main --config data/midasConfig.xml data/rc_3m5t.xml --agent edu.cwru.SimpleRTS.agent.RCAgent 0
+#java -cp SimpleRTSv3.1.jar edu.cwru.SimpleRTS.Main --config data/midasConfig.xml data/rc_3m5t.xml --agent edu.cwru.SimpleRTS.agent.RCAgent 0 --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam true --agentparam true
