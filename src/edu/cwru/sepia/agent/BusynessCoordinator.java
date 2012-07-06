af9c6fa2e64ebdb68d1c03b095837cb9d9989673
Feng Cao <fxc100@case.edu>
2012-07-06 20:57:11 +0000

    Added copyright and GPL licensing to header of each files; added COPYING file

--- src/edu/cwru/sepia/agent/BusynessCoordinator.java
+++ src/edu/cwru/sepia/agent/BusynessCoordinator.java
@@ -1,3 +1,22 @@
+/**
+ *  Strategy Engine for Programming Intelligent Agents (SEPIA)
+    Copyright (C) 2012 Case Western Reserve University
+
+    This file is part of SEPIA.
+
+    SEPIA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SEPIA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SEPIA.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package edu.cwru.sepia.agent;
 
 import java.io.Serializable;
