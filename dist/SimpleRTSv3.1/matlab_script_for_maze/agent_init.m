52e3e919c68ededf8a427d26a2217b7f2fe039a7
Kyle Marek <kyle.marek@case.edu>
2021-02-05 11:47:00 -0500

    Remove SimpleRTSv3.1 distribution
    These files are now tracked in my SimpleRTS repo.

--- dist/SimpleRTSv3.1/matlab_script_for_maze/agent_init.m
+++
@@ -1,9 +0,0 @@
-% In this script you initialize the variables to be used for your agent.
-% This script will be called only one time at the beginning of the program.
-function [] = agent_init(arg_playernum)
-
-global playernum;
-
-playernum = arg_playernum;
-
-end
--- dist/SimpleRTSv3.1/matlab_script_for_maze/agent_terminalStep.m
+++
@@ -1,8 +0,0 @@
-% This function will be called at the end of each episode.
-% Nothing is returned here.
-function [] = agent_terminalStep( state )
-
-fprintf('This episode is finished!');
-
-end
-
